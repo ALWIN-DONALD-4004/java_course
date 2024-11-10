2f19a83ee4c750e695fbbf8a3da5be31dcdb8c2f
alwindonald <alwindonald4004@gmail.com>
2024-11-10 22:38:27 +0530

    commit1

---
+++ CodingChallenges/src/com/emc/coding/challengetwo/parttwo/Two.java
@@ -0,0 +1,24 @@
+package com.emc.coding.challengetwo.parttwo;
+import java.util.*;
+public class Two {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int sub1,sub2,sub3,sub4,sub5;
+		
+		System.out.print("Enter the first mark: ");
+	    sub1 = sc.nextInt();
+	        
+	    System.out.print("Enter the second mark: ");
+	    sub2 = sc.nextInt();
+	        
+	    System.out.print("Enter the third mark: ");
+	    sub3 = sc.nextInt();
+	        
+	    System.out.print("Enter the fourth mark: ");
+	    sub4 = sc.nextInt();
+	        
+	    System.out.print("Enter the fifth mark: ");
+	    sub5 = sc.nextInt();
+	}
+}
