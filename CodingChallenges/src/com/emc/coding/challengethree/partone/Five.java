66d473d8aff7583f0525e1c806ea8d16362bd91e
alwindonald <alwindonald4004@gmail.com>
2024-11-12 21:21:41 +0530

    commitTwo

--- CodingChallenges/src/com/emc/coding/challengethree/partone/Five.java
+++ CodingChallenges/src/com/emc/coding/challengethree/partone/Five.java
@@ -1,16 +1,16 @@
-package com.emc.coding.challengethree.partone;
-
-public class Five {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		for(int i = 1 ; i<=100 ; i++) {
-			if(i%3 == 0 && i%5 == 0) {
-				System.out.println(i);
-			}
-		}
-
-	}
-
-}
+//package com.emc.coding.challengethree.partone;
+//
+//public class Five {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		
+//		for(int i = 1 ; i<=100 ; i++) {
+//			if(i%3 == 0 && i%5 == 0) {
+//				System.out.println(i);
+//			}
+//		}
+//
+//	}
+//
+//}
