3e5ad0d8549d7095ebc310055ad21c148fffa494
alwindonald <alwindonald4004@gmail.com>
2024-11-12 21:22:14 +0530

    committhree

--- CodingChallenges/src/com/emc/coding/challengethree/partone/Three.java
+++ CodingChallenges/src/com/emc/coding/challengethree/partone/Three.java
@@ -1,19 +1,19 @@
-//package com.emc.coding.challengethree.partone;
-//
-//public class Three {
-//
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//		int count = 0;
-//		for (int i = 1; i<=10 ; i++) {
-//			
-//			if(i%2 != 0) {
-//				System.out.println(i);
-//				count+=1;
-//			}		
-//		}
-//		System.out.println("total count = " + count);
-//
-//	}
-//
-//}
+package com.emc.coding.challengethree.partone;
+
+public class Three {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count = 0;
+		for (int i = 1; i<=10 ; i++) {
+			
+			if(i%2 != 0) {
+				System.out.println(i);
+				count+=1;
+			}		
+		}
+		System.out.println("total count = " + count);
+
+	}
+
+}
