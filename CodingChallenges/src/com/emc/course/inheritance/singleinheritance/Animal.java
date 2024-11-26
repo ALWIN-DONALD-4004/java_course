9379eb27754272e3eefcd1d3db158fc3782b199e
alwindonald <alwindonald4004@gmail.com>
2024-11-26 22:04:08 +0530

    commit

--- CodingChallenges/src/com/emc/course/inheritance/singleinheritance/Animal.java
+++ CodingChallenges/src/com/emc/course/inheritance/singleinheritance/Animal.java
@@ -1,5 +1,6 @@
 package com.emc.course.inheritance.singleinheritance;
 
+// Parent or super Class
 public class Animal {
 	public void eat(){
 		System.out.println("this animal can eat");
--- CodingChallenges/src/com/emc/course/inheritance/singleinheritance/Dog.java
+++ CodingChallenges/src/com/emc/course/inheritance/singleinheritance/Dog.java
@@ -1,5 +1,6 @@
 package com.emc.course.inheritance.singleinheritance;
 
+//Derived or sub or child class
 public class Dog extends Animal {
 	public void bark() {
 		System.out.println("Barks");
