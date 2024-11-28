e258975e400a4c1380bb67fd32e1bd7cfa7cd380
alwindonald <alwindonald4004@gmail.com>
2024-11-28 22:01:05 +0530

    static commit

--- CodingChallenges/src/com/emc/course/keyword/statickeyword/Demo.java
+++ CodingChallenges/src/com/emc/course/keyword/statickeyword/Demo.java
@@ -3,6 +3,10 @@ package com.emc.course.keyword.statickeyword;
 class Student{
 	static String dept = "ECE";
 	String name;
+	
+	static void disp() {
+		System.out.println("Hello");
+	}
 }
 
 public class Demo {
@@ -15,10 +19,12 @@ public class Demo {
 		// TODO Auto-generated method stub
 		Student s1 = new Student();
 		
+		Student.disp();
 		//Student.dept = "AIDS";
 		s1.name = "Alwin donald";
 		System.out.println(s1.name+"\n"+s1.dept);
 		
+		Student.disp();
 		Student s2 = new Student();
 		s2.name = "mani";
 		System.out.println(s2.name+"\n"+s2.dept);
