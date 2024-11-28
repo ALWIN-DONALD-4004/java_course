017c4c96a4305ed78b489f80f9af07c8481dc726
alwindonald <alwindonald4004@gmail.com>
2024-11-28 21:56:34 +0530

    static

---
+++ CodingChallenges/src/com/emc/course/keyword/statickeyword/Demo.java
@@ -0,0 +1,29 @@
+package com.emc.course.keyword.statickeyword;
+
+class Student{
+	static String dept = "ECE";
+	String name;
+}
+
+public class Demo {
+	
+	static {
+		System.out.println("welcome to IFET College Of Engineering");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s1 = new Student();
+		
+		//Student.dept = "AIDS";
+		s1.name = "Alwin donald";
+		System.out.println(s1.name+"\n"+s1.dept);
+		
+		Student s2 = new Student();
+		s2.name = "mani";
+		System.out.println(s2.name+"\n"+s2.dept);
+	
+
+	}
+
+}
