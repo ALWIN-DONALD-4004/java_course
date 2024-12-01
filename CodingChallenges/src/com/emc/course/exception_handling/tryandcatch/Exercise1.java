9204a58c9fd2cc3e9759c99689626bffeb28a563
alwindonald <alwindonald4004@gmail.com>
2024-12-01 21:19:58 +0530

    try and catch

---
+++ CodingChallenges/src/com/emc/course/exception_handling/tryandcatch/Exercise1.java
@@ -0,0 +1,36 @@
+package com.emc.course.exception_handling.tryandcatch;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exercise1 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int a = 0;
+		int b = 0;
+		int c = 0;
+		try {
+			a = sc.nextInt();
+			b = sc.nextInt();
+			c = a/b;
+		}
+		catch(ArithmeticException e) {
+			System.out.println("please enter the b value greater than 0");
+		}
+		
+		catch(InputMismatchException e) {
+			System.out.println("enter the input as integer");
+		}
+		if(c!=0) {
+			System.out.println(c);
+		}
+		
+		
+		
+	}
+
+}
---
+++ CodingChallenges/src/com/emc/course/exception_handling/tryandcatch/tryExecuter.java
@@ -0,0 +1,32 @@
+package com.emc.course.exception_handling.tryandcatch;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class tryExecuter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			int x = sc.nextInt();
+			int y = 10;
+		}
+//		catch(Exception e) {
+////			System.out.println(e);
+//			System.out.println("Exception handled");
+//		}
+		
+		catch(InputMismatchException e) {
+			System.out.println(e);
+		}
+		catch(ArithmeticException e1) {
+			System.out.println(e1);
+		}
+		
+		System.out.println("program completle executed ......");
+
+	}
+
+}
